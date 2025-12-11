712ba41ae00df24b324cff48e6576d58e915cde6
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-11 16:02:50 +0530

    Create Twosum.py

---
+++ Twosum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums, target):
+        num_map = {}  # Dictionary to store numbers and their indices
+        
+        for i, num in enumerate(nums):
+            complement = target - num  # Find the complement
+            
+            if complement in num_map:
+                return [num_map[complement], i]  # Return indices
+            
+            num_map[num] = i  # Store index of current number
+        
+        return []  # As per problem constraints, this won't be reached
