6c5ef9649371b69d9c6c195bbda69dc9b11fbb9d
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-11 16:09:14 +0530

    Create Roman_to_Integer.py

---
+++ Roman_to_Integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def romanToInt(self, s):
+        values = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev = 0
+        
+        for ch in reversed(s):  # loop from right to left
+            val = values[ch]
+            
+            if val < prev:
+                total -= val
+            else:
+                total += val
+            
+            prev = val
+        
+        return total
