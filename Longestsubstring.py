e9f2003db8a9bfb6fea1d8bbbf3b5ea0e6fc9f8d
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-12 15:14:19 +0530

    Update Longestsubstring.py

--- Longestsubstring.py
+++ Longestsubstring.py
@@ -6,7 +6,7 @@ class Solution(object):
 
         for right, char in enumerate(s):
             if char in seen and seen[char] >= left:
-                left = seen[char] + 1
+                left = seen[char] + 1 
 
             seen[char] = right
             max_len = max(max_len, right - left + 1)
