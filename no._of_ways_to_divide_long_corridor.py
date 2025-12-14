4efd2a95e0270da66a7ed310b6620bec13daba03
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-14 15:24:37 +0530

    Create no._of_ways_to_divide_long_corridor.py

---
+++ no._of_ways_to_divide_long_corridor.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def numberOfWays(self, corridor):
+        MOD = 10**9 + 7
+
+        total_seats = corridor.count('S')
+        if total_seats == 0 or total_seats % 2 != 0:
+            return 0
+
+        ways = 1
+        seats = 0
+        plants = 0
+
+        for c in corridor:
+            if c == 'S':
+                seats += 1
+                # after completing one section of 2 seats
+                if seats == 3:
+                    ways = (ways * (plants + 1)) % MOD
+                    plants = 0
+                    seats = 1
+            else:  # c == 'P'
+                if seats == 2:
+                    plants += 1
+
+        return ways
