b2250667240e2ac6ff4235d028d511f6f7411f3e
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-20 01:10:02 +0530

    Create Remove_Element.py

---
+++ Remove_Element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        left = 0
+        right = len(nums)
+
+        while left < right:
+            if nums[left] == val:
+                nums[left] = nums[right - 1]
+                right -= 1
+            else:
+                left += 1
+
+        return right
