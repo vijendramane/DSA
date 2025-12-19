8dc529b5558a728bcb68bff4cdd89635d8a7a0a0
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-19 12:13:06 +0530

    Create Remove_Duplicates.py

---
+++ Remove_Duplicates.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        k = 1  # index for next unique element
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
