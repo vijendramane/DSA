b628809bca32c3809e6b4c9a57178e81d7da820c
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-24 16:08:23 +0530

    Create length_of_last_word.py

---
+++ length_of_last_word.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        length = 0
+        i = len(s) - 1
+
+        # skip trailing spaces
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+
+        # count characters of last word
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+
+        return length
