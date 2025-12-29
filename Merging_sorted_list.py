c8d8ed0ae88991e58fabf7789cf2653be2f6a347
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-30 00:34:04 +0530

    Update Merging_sorted_list.py

--- Merging_sorted_list.py
+++ Merging_sorted_list.py
@@ -2,7 +2,7 @@
 # class ListNode(object): 
 #     def __init__(self, val=0, next=None):
 #         self.val = val
-#         self.next = next
+#         self.next = next 
 
 class Solution(object): 
     def mergeTwoLists(self, list1, list2):
