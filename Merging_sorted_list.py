0eaa85a8b673af748195beb7402b7ee81abf528f
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-30 00:34:34 +0530

    Update Merging_sorted_list.py

--- Merging_sorted_list.py
+++ Merging_sorted_list.py
@@ -1,7 +1,7 @@
  # Definition for singly-linked list.
 # class ListNode(object): 
 #     def __init__(self, val=0, next=None):
-#         self.val = val
+#         self.val = val 
 #         self.next = next 
 
 class Solution(object): 
