8d9d8566d7c9940c0696943ce24e53599a1da5dc
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2025-12-29 13:53:50 +0530

    Create Zigzag_conversion.py

---
+++ Zigzag_conversion.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [""] * numRows
+        currentRow = 0
+        direction = 1  # 1 = down, -1 = up
+
+        for ch in s:
+            rows[currentRow] += ch
+
+            # change direction at top or bottom
+            if currentRow == 0:
+                direction = 1
+            elif currentRow == numRows - 1:
+                direction = -1
+
+            currentRow += direction
+
+        return "".join(rows)
+
