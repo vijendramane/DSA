24317a800bf2c9585b0eafe0720bf5ab3b197bf7
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-01-09 00:27:02 +0530

    Update Remove_Element.py

--- Remove_Element.py
+++ Remove_Element.py
@@ -3,7 +3,7 @@
         left = 0
         right = len(nums)
 
-        while left < right:
+        while left < right: 
             if nums[left] == val:
                 nums[left] = nums[right - 1]
                 right -= 1
