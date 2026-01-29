0145750b7be88e129ad143325c65723ae733ba99
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-01-29 23:02:38 +0530

    Update Remove_Duplicates.py

--- Remove_Duplicates.py
+++ Remove_Duplicates.py
@@ -5,7 +5,7 @@ class Solution(object):
 
         k = 1  # index for next unique element  
         
-        for i in range(1, len(nums)):
+        for i in range(1, len(nums)): 
             if nums[i] != nums[i - 1]:
                 nums[k] = nums[i]
                 k += 1
