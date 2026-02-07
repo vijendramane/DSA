086d7fa12262f1719b43ae1ab2b056d7279e991c
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-02-08 01:22:23 +0530

    Update Remove_Element.py

--- Remove_Element.py
+++ Remove_Element.py
@@ -7,7 +7,7 @@
       
             if nums[left] == val: 
                 nums[left] = nums[right - 1] 
-                right -= 1
+                right -= 1 
             else:
                 left += 1 S
                 
