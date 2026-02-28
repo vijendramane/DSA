5a6c938f30131b9ca1ee12989c99b5118e5fdad5
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-02-28 22:01:36 +0530

    Update Merging_sorted_list.py

--- Merging_sorted_list.py
+++ Merging_sorted_list.py
@@ -7,7 +7,7 @@
 class Solution(object):   
     def mergeTwoLists(self, list1, list2):
         dummy = ListNode(-1) 
-        current = dummy
+        current = dummy 
  
         while list1 and list2:
             if list1.val <= list2.val:
