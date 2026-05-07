5b5301c9d875bfe9161b0e95c8afd5ee806fade3
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-05-07 19:50:17 +0530

    Update Hashmap.py

--- Hashmap.py
+++ Hashmap.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, key, value):
         self.key = key
-        self.value = value 
+        self.value = value  
         self.prev = None
         self.next = None
  
