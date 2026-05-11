4134c48bf44655cdddc341607f636c48667ab4fd
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-05-11 23:59:25 +0530

    Update Longestsubstring.py

--- Longestsubstring.py
+++ Longestsubstring.py
@@ -6,7 +6,7 @@ class Solution(object):
         max_len = 0
       
         for right, char in enumerate(s):
-            if char in seen and seen[char] >= left
+            if char in seen and seen[char] >= left:
                 left = seen[char] + 1
 
             seen[char] = right 
