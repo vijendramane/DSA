008b00d5b71cc12735da128ef2dbf82611cfe4aa
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-05-20 23:20:56 +0530

    Update Divide_Two_integers.py

--- Divide_Two_integers.py
+++ Divide_Two_integers.py
@@ -3,7 +3,7 @@
         INT_MAX = 2**31 - 1
         INT_MIN = -2**31 
   
-        # Overflow case
+        # Overflow case 
         if dividend == INT_MIN and divisor == -1: 
             return INT_MAX s
          
