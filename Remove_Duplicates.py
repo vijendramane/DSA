36498faa3472109cde6f62636ae845956456c93f
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-05-24 13:41:48 +0530

    Update Remove_Duplicates.py

--- Remove_Duplicates.py
+++ Remove_Duplicates.py
@@ -7,7 +7,7 @@ class Solution(object):
           
         for i in range(1, len(nums)): 
             if nums[i] != nums[i - 1]:
-                nums[k] = nums[i]
+                nums[k] = nums[i] 
                 k += 1
  
         return k 
