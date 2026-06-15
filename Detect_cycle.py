a527e72e85d2872e463d02c0de083e7b75630354
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-06-16 02:05:44 +0530

    Create Detect_cycle.py

---
+++ Detect_cycle.py
@@ -0,0 +1,21 @@
+class Node:
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def has_cycle(head):
+
+    slow = head
+    fast = head
+
+    while fast and fast.next:
+
+        slow = slow.next
+        fast = fast.next.next
+
+        if slow == fast:
+            return True
+
+    return False
