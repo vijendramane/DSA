5a9905fbaadfd868bf3c54e880cdaf900d1e7e2d
Vijendra Mane <148106020+vijendramane@users.noreply.github.com>
2026-09-15 07:43:35 +0530

    Update Roman_to_Integer.py

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -3,10 +3,10 @@ class Solution(object):
         values = {
             'I': 1, 'V': 5, 'X': 10,
             'L': 50, 'C': 100, 'D': 500, 'M': 1000
-        }
+        } 
         
         total = 0
-        prev = 0 
+        prev = 0
         
         for ch in reversed(s):  # loop from right to left
             val = values[ch]
